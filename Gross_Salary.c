bd1972290387d7a61686062c3254572e96eaf0c3
sarankumar0101 <119718662+sarankumar0101@users.noreply.github.com>
2022-12-08 10:26:40 +0530

    #include<stdio.h> int main() {     int bs;     scanf("%d",&bs);     float da,hra,gs;     if(bs<=10000)     {         da=0.8*bs;         hra=0.2*bs;     }     else if(bs<=20000)     {         da=0.9*bs;         hra=0.25*bs;     }     else if(bs>20000)     {         da=0.95*bs;         hra=0.3*bs;     }     gs=bs+da+hra;     {         printf("%0.2f",gs);     } }

---
+++ Gross_Salary.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int bs;
+    scanf("%d",&bs);
+    float da,hra,gs;
+    if(bs<=10000)
+    {
+        da=0.8*bs;
+        hra=0.2*bs;
+    }
+    else if(bs<=20000)
+    {
+        da=0.9*bs;
+        hra=0.25*bs;
+    }
+    else if(bs>20000)
+    {
+        da=0.95*bs;
+        hra=0.3*bs;
+    }
+    gs=bs+da+hra;
+    {
+        printf("%0.2f",gs);
+    }
+}
